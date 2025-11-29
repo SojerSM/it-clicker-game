44bf7e71160caceebaa366bb5c13347b813f5ed7
sojer <seb.maz1996@gmail.com>
2025-11-30 00:20:39 +0100

    Start generating new ticket instantly if the last ticket was solved

--- src/app/domains/progress/tickets/services/ticket.service.ts
+++ src/app/domains/progress/tickets/services/ticket.service.ts
@@ -3,13 +3,15 @@ import { Ticket } from '../types/ticket.model';
 import { ResourcesService } from '../../../resources/resources.service';
 import { ProjectService } from '../../projects/services/project.service';
 import { GameStateService } from '../../../../core/services/game-state.service';
+import { TicketBuilderService } from './ticket-builder.service';
 
 @Injectable({ providedIn: 'root' })
 export class TicketService {
   constructor(
     private resourceService: ResourcesService,
     private projectService: ProjectService,
-    private gameStateService: GameStateService
+    private gameStateService: GameStateService,
+    private ticketBuilderService: TicketBuilderService
   ) {
     effect(() => {
       this.gameStateService.ticketState().active.forEach((ticket) => {
@@ -42,6 +44,13 @@ export class TicketService {
     this.gameStateService.updateTicket((state) => {
       state.active = state.active.filter((t) => t.id !== ticket.id);
     });
+
+    if (this.gameStateService.ticketState().active.length === 0) {
+      const project = this.gameStateService.projectState().current;
+      const newTicket = this.ticketBuilderService.getRandomTicket(project);
+
+      this.addTicket(newTicket);
+    }
   }
 
   private grantReward(ticket: Ticket): void {
