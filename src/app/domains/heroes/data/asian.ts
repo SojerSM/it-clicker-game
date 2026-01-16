2626b76c62651f75f16a1fedd71a53e13d4a25e0
sojer <seb.maz1996@gmail.com>
2026-01-16 18:27:08 +0100

    Create initial hero parts to build random heroes with individual statistics

---
+++ src/app/domains/heroes/data/asian.ts
@@ -0,0 +1,88 @@
+export const MALE_HERO_NAMES_ASIA: string[] = [
+  'Hiroshi',
+  'Kenji',
+  'Takumi',
+  'Yuki',
+  'Ryo',
+  'Minho',
+  'Joon',
+  'Taeyang',
+  'Wei',
+  'Jun',
+  'Liang',
+  'Chen',
+  'Akira',
+  'Kim',
+  'Subaru',
+];
+
+export const FEMALE_HERO_NAMES_ASIA: string[] = [
+  'Yui',
+  'Aiko',
+  'Sakura',
+  'Mei',
+  'Ling',
+  'Xia',
+  'Hana',
+  'Minji',
+  'Soojin',
+  'Jisoo',
+  'Ayumi',
+  'Naomi',
+  'Rin',
+  'Mika',
+  'Toyota',
+];
+
+export const SURNAMES_ASIA: string[] = [
+  'Tanaka',
+  'Suzuki',
+  'Sato',
+  'Watanabe',
+  'Nakamura',
+  'Kim',
+  'Lee',
+  'Park',
+  'Choi',
+  'Zhang',
+  'Wang',
+  'Li',
+  'Chen',
+  'Liu',
+  'Huang',
+  'Yamamoto',
+  'Kobayashi',
+  'Kang',
+  'Shin',
+  'Zhao',
+];
+
+export const EDUCATION_PRESTIGIOUS_ASIA: string[] = [
+  'University of Tokyo',
+  'Kyoto University',
+  'Seoul National University',
+  'Peking University',
+  'Tsinghua University',
+];
+
+export const EDUCATION_REGULAR_ASIA: string[] = [
+  'Osaka University',
+  'Keio University',
+  'Yonsei University',
+  'Fudan University',
+  'National Taiwan University',
+];
+
+export const EDUCATION_MEME_ASIA: string[] = [
+  'Sushi Rolling School',
+  'Bullshitsu Martial Academy',
+  'Ramen & Relaxation University',
+  'Origami Folding School of Precision',
+  'Dragon Breath Firefighting School',
+  'Samurai Self-Discipline School',
+  'Zen Meditation & Procrastination Institute',
+  'K-Pop Dance Studio',
+  'Chopstick Engineering University',
+  'Sake & Strategy School',
+  'None',
+];
---
+++ src/app/domains/heroes/data/english.ts
@@ -0,0 +1,83 @@
+export const MALE_HERO_NAMES_EN: string[] = [
+  'James',
+  'Michael',
+  'John',
+  'Robert',
+  'Bill',
+  'Christopher',
+  'Matthew',
+  'Joshua',
+  'Andrew',
+  'David',
+  'Joseph',
+  'Brian',
+  'Peter',
+];
+
+export const FEMALE_HERO_NAMES_EN: string[] = [
+  'Emily',
+  'Sarah',
+  'Jessica',
+  'Ashley',
+  'Amanda',
+  'Jennifer',
+  'Elizabeth',
+  'Lauren',
+  'Betty',
+  'Chloe',
+  'Amy',
+  'Ivy',
+  'Gwen',
+  'Maggie',
+];
+
+export const SURNAMES_EN: string[] = [
+  'Smith',
+  'Johnson',
+  'Brown',
+  'Goldberg',
+  'Anderson',
+  'Thompson',
+  'Walker',
+  'Harris',
+  'Clark',
+  'Lewis',
+  'Robinson',
+  'Hall',
+  'Young',
+  'Stein',
+  'Wright',
+  'Scott',
+  'Gates',
+  'Baker',
+  'Adams',
+  'Turner',
+];
+
+export const EDUCATION_PRESTIGIOUS_EN: string[] = [
+  'Harvard University',
+  'Massachusetts Institute of Technology',
+  'Stanford University',
+  'University of Oxford',
+  'University of Cambridge',
+];
+
+export const EDUCATION_REGULAR_EN: string[] = [
+  'University of Michigan',
+  'Pennsylvania State University',
+  'University of Texas',
+  'Ohio State University',
+  'University of London',
+];
+
+export const EDUCATION_MEME_EN: string[] = [
+  'YouTube Academy of Success',
+  'McDonalds Internship',
+  'Vibe Coding Udemy Course',
+  'Night Shift Learning Center',
+  'StackOverflow University',
+  'Wikipedia School of Everything',
+  'Texas State Prison',
+  'Netflix & Chill',
+  'None',
+];
