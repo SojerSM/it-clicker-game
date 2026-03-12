430f8bbbf8b4da3455ba875e95169315c7384ceb
sojer <seb.maz1996@gmail.com>
2026-03-12 13:48:23 +0100

    Add translation to register page

--- src/app/layout/landing/register/register-form/register-form.component.ts
+++ src/app/layout/landing/register/register-form/register-form.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-register-form',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './register-form.component.html',
-  styleUrl: './register-form.component.scss'
+  styleUrl: './register-form.component.scss',
 })
-export class RegisterFormComponent {
-
-}
+export class RegisterFormComponent {}
--- src/app/layout/landing/register/register.component.ts
+++ src/app/layout/landing/register/register.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { LangWidgetComponent } from '../../../shared/components/lang-widget/lang-widget.component';
 import { PageComponent } from '../../../shared/components/page/page.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterOutlet, LangWidgetComponent, PageComponent, RegisterFormComponent],
+  imports: [LangWidgetComponent, PageComponent, RegisterFormComponent, TranslatePipe],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
